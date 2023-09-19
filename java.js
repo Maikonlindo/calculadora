e6d14972be93deec1cb7dda2a4d8c1b3c1399257
Maikonlindo <137800939+Maikonlindo@users.noreply.github.com>
2023-09-19 11:47:11 -0300

    Initial commit

---
+++ java.js
@@ -0,0 +1,12 @@
+calcule.addEventListener("click", function () {
+  var a = ValorA.value;
+  var b = ValorB.value;
+  //var op = operacao.value;
+
+  a = parseInt(a);
+
+  b = parseInt(b);
+  var result = a + b;
+
+  alert(result);
+});
